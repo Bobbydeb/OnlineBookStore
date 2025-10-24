e09477295def39e539df477e381be3403c3d365f
Bobbydeb <autochess1124@gmail.com>
2025-10-25 00:46:45 +0700

    Add project files.

---
+++ OnlineBookStore/MonthlySales.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MonthlySales.aspx.cs" Inherits="OnlineBookStore.MonthlySales" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Monthly Sales Report</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="width:80%; margin:auto; text-align:center;">
+            <h2>ยอดขายรายเดือน (Monthly Sales)</h2>
+            <asp:GridView ID="GridView1" runat="server" 
+                AutoGenerateColumns="true" 
+                BorderColor="Gray" 
+                BorderWidth="1px"
+                CellPadding="6"
+                GridLines="Both"
+                Font-Names="Segoe UI" 
+                Font-Size="Medium">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ OnlineBookStore/MonthlySales.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace OnlineBookStore
+{
+    public partial class MonthlySales : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
+        }
+
+        private void LoadData()
+        {
+            // ดึง connection string จาก Web.config
+            string connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string sql = @"SELECT FORMAT(o.OrderDate, 'yyyy-MM') AS OrderMonth,
+                                      SUM(o.TotalAmount) AS MonthlySales
+                               FROM OrderTable o
+                               GROUP BY FORMAT(o.OrderDate, 'yyyy-MM')
+                               ORDER BY OrderMonth";
+
+                SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
